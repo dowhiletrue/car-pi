d08b06bd009a8c75ba07d43b9a3834d9d2ea761d
Michael Zaugg <mr.somebody@gmx.ch>
2018-02-19 20:15:57 +0000

    add initial untested btplayer

--- setup.sh
+++ setup.sh
@@ -7,3 +7,15 @@ dtoverlay=spi-bcm2835-overlay
 
 EOF
 
+apt install -y bluez pulseaudio pulseaudio-module-bluetooth pulseaudio-module-zeroconf
+
+cp /etc/bluetooth/main.conf /etc/bluetooth/main.conf.orig
+cp kodi-a2dp/main.conf /etc/bluetooth/main.conf
+
+cp kodi-a2dp/audio.conf /etc/bluetooth/audio.conf
+
+cp /etc/dbus-1/system.d/bluetooth.conf /etc/dbus-1/system.d/bluetooth.conf.orig
+cp kodi-a2dp/bluetooth.conf /etc/dbus-1/system.d/bluetooth.conf
+
+cp /etc/pulse/system.pa /etc/pulse/system.pa.orig
+cp kodi-a2dp/system.pa /etc/pulse/system.pa
