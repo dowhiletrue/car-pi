a62fff236ae4beef4fadc75ddb9009c873b9417e
Michael Zaugg <mr.somebody@gmx.ch>
2018-02-24 08:30:25 +0100

    fix locale

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 ## locale
-locale-gen de_CH.UTF-8
-echo 'LANG="en_US.utf8"' > /etc/default/locale
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "de_CH.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 
 cat << EOF > /etc/default/keyboard
 XKBMODEL="pc105"
